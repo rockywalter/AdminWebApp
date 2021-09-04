7f7f7e38b27359642d3b0c34f2afe0d06ce94d82
rockywalter <51148383+rockywalter@users.noreply.github.com>
2021-09-04 11:16:57 +0530

    Remove unwanted files v1

---
+++ index.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html>
+<?php include 'head.php';?>
+<body id="page-top">
+  <!-- Page Wrapper -->
+  <div id="wrapper">
+
+  <?php include 'sidebar.php';?>
+  
+    <!-- Content Wrapper -->
+    <div id="content-wrapper" class="d-flex flex-column">
+     <!-- Main Content -->
+      <div id="content">
+   
+       <?php include 'topbar.php';?>
+      
+       <!-- Begin Page Content -->
+        <div class="container-fluid">
+
+          <!-- Page Heading -->
+          <h1 class="h3 mb-4 text-gray-800">Hello Page</h1>
+
+ <!-- Begin Here to write-->
+
+<p>  hello world icne ceciuhco ceocheocnle cei         </p>
+
+
+
+
+
+
+        </div>
+        <!-- /.container-fluid -->
+           
+      </div>
+      <!-- End of Main Content -->
+      
+       <?php include 'footer.php';?>
+     
+    </div>
+    <!-- End of Content Wrapper -->
+  
+  </div>
+  <!-- End of Page Wrapper -->
+   <?php include 'end.php';?>
+ 
+</body>
+</html>
---
+++ sidebar.php
@@ -0,0 +1,102 @@
+    <!-- Sidebar -->
+    <ul class="navbar-nav bg-gradient-primary sidebar sidebar-dark accordion" id="accordionSidebar">
+
+      <!-- Sidebar - Brand -->
+      <a class="sidebar-brand d-flex align-items-center justify-content-center" href="halls">
+      
+        <div class="sidebar-brand-text mx-3">SASIP INSTITUTE</div>
+      </a>
+
+      <!-- Divider -->
+      <hr class="sidebar-divider my-0">
+
+     
+
+      <!-- Divider -->
+      <hr class="sidebar-divider">
+
+      <!-- Nav Item - Dashboard -->
+      <li class="nav-item">
+        <a class="nav-link" href="halls">
+          <i class="fas fa-fw fa-tachometer-alt"></i>
+          <span>Halls</span></a>
+      </li>
+
+      <!-- Divider -->
+      <hr class="sidebar-divider">
+
+       <!-- Nav Item - Dashboard -->
+      <li class="nav-item">
+        <a class="nav-link" href="grade">
+          <i class="fas fa-fw fa-tachometer-alt"></i>
+          <span>Grade</span></a>
+      </li>
+
+      <!-- Divider -->
+      <hr class="sidebar-divider">
+
+       <!-- Nav Item - Dashboard -->
+      <li class="nav-item">
+        <a class="nav-link" href="subject">
+          <i class="fas fa-fw fa-tachometer-alt"></i>
+          <span>Subject</span></a>
+      </li>
+
+      <!-- Divider -->
+      <hr class="sidebar-divider">
+
+       <!-- Nav Item - Dashboard -->
+      <li class="nav-item">
+        <a class="nav-link" href="teacher">
+          <i class="fas fa-fw fa-tachometer-alt"></i>
+          <span>Teacher</span></a>
+      </li>
+      
+         <!-- Divider -->
+      <hr class="sidebar-divider">
+
+       <!-- Nav Item - Dashboard -->
+      <li class="nav-item">
+        <a class="nav-link" href="srouting">
+          <i class="fas fa-fw fa-tachometer-alt"></i>
+          <span>Subject Routing</span></a>
+      </li>
+        <hr class="sidebar-divider">
+  <!-- Divider -->
+   <!-- Nav Item - Dashboard -->
+      <li class="nav-item">
+        <a class="nav-link" href="student">
+          <i class="fas fa-fw fa-tachometer-alt"></i>
+          <span>Students</span></a>
+      </li>
+  
+      <!-- Divider -->
+        <hr class="sidebar-divider">
+        <!-- Nav Item - Dashboard -->
+      <li class="nav-item">
+        <a class="nav-link" href="exam">
+          <i class="fas fa-fw fa-tachometer-alt"></i>
+          <span>Exams</span></a>
+      </li>
+      
+      <hr class="sidebar-divider">   
+         <li class="nav-item">
+        <a class="nav-link" href="resultexam">
+          <i class="fas fa-fw fa-tachometer-alt"></i>
+          <span>Result of exams</span></a>
+      </li>
+      
+      
+      
+       
+      
+      
+      <hr class="sidebar-divider d-none d-md-block">
+
+      <!-- Sidebar Toggler (Sidebar) -->
+      <div class="text-center d-none d-md-inline">
+        <button class="rounded-circle border-0" id="sidebarToggle"></button>
+      </div>
+
+    </ul>
+    <!-- End of Sidebar -->
